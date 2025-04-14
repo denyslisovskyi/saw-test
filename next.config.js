c09b9233aac3dbf20640522585175cfca59a0a90
denys.lisovskyi <lisovskyi.d@gmail.com>
2025-04-14 20:42:33 +0300

    fix: images & fonts cache optimization

--- next.config.js
+++ next.config.js
@@ -1,13 +1,38 @@
 const nextConfig = {
   images: {
-    unoptimized: true,
+    formats: ["image/webp"],
+    dangerouslyAllowSVG: true,
   },
   experimental: {
+    serverActions: true,
     turbo: {
       enabled: false,
     },
   },
-  matcher: "/((?!_next|favicon.ico).*)",
+  async headers() {
+    return [
+      {
+        // Images cache
+        source: "/(.*)\\.(jpg|jpeg|png|gif|webp|svg)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        // Fonts cache
+        source: "/(.*)\\.(woff|woff2|ttf|otf)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {

7a6a5d7ba05f58b82d511ea28839789b6af6816c
Lisovskyi Denys <lisovskyi.d@gmail.com>
2025-04-20 23:36:12 +0200

    fix: moved GTM to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Footer } from "@/components/Footer";
 import TrackParams from "@/components/TrackParams";
 import type { Metadata } from "next";
 import { defaultMeta } from "@/lib/seo";
-import Script from "next/script";
+import GTM from "@/components/GTM";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://saw-test-denys-projects-9a46b281.vercel.app"),
@@ -29,26 +29,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="uk">
-      <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P5DBN4S2');
-          `}
-        </Script>
-      </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P5DBN4S2"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        <GTM />
         <TrackParams />
         <main className="flex-grow">{children}</main>
         <Footer />
